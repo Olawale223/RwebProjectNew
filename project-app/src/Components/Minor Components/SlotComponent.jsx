a4eb84bf9810abbd82adc362d388eb2144155e17
ola wale <90061003+Olawale223@users.noreply.github.com>
2026-04-06 11:41:04 +0100

    update

--- project-app/src/Components/Minor Components/SlotComponent.jsx
+++ project-app/src/Components/Minor Components/SlotComponent.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './SlotComponent.css';
-import roleContent from '../Roles'
 
-export default function SlotComponent({image, name, role}) {
+export default function SlotComponent({ image, name, role }) {
   return (
     <div className="team-section">
       <div className='member-image-container'>
-      <img src={image} alt={name} className='member-image'/>
+        <img src={image} alt={name} className='member-image' />
       </div>
       <div className="team-member">
         <h3>{name}</h3>
